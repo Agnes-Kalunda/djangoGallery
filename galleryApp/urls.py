6d2eef4ad1b700f74d0de3fa8e00e70da6e2c156
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-05-30 01:11:02 +0300

    added home routing

--- galleryApp/urls.py
+++ galleryApp/urls.py
@@ -2,17 +2,16 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
-app_name = 'galleryApp'
-
+app_name = 'gallery'
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
     path('nav/',views.nav,name = 'nav'),
     path('base/',views.base,name = 'base'),
 
-    path(r'^search/', views.search_results, name='search'),
-    path(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
+    path('search/', views.search_results, name='search'),
+    path('location/', views.image_location, name='location'),
 
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
