81b492285b34c2c6aec040aa6ab96d0fed23287c
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-05-30 00:48:32 +0300

    added search routes

--- galleryApp/urls.py
+++ galleryApp/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
+app_name = 'effecto'
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
     path('nav/',views.nav,name = 'nav'),
     path('base/',views.base,name = 'base'),
 
+    path(r'^search/', views.search_results, name='search'),
+    path(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
