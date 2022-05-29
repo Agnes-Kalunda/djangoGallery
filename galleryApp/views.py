71921d2f89d0819a276c9d33f562821afc70f695
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-05-30 00:13:29 +0300

    added navbar styyling

--- galleryApp/views.py
+++ galleryApp/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render
 
 
 def welcome(request):
-    return render (request,'home.html')
+    return render (request,'home.html' )
 
 
 # Create your views here.
 def nav(request):
     return render (request,'navbar.html' )
-
 def base(request):
     return render (request,'base.html' )
